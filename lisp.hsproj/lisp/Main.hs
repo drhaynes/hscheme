b05be170de5f1aca2b9da2b38dffe699d479b6bb
drhaynes <drhmbp@gmail.com>
2018-07-08 00:46:46 +0100

    Add symbol parsing

--- lisp.hsproj/lisp/Main.hs
+++ lisp.hsproj/lisp/Main.hs
@@ -1,7 +1,17 @@
 module Main where
+import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
+
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_-"
+
+readExpr :: String -> String
+readExpr input = case parse symbol "lisp" input of
+  Left err -> "No symbol match: " ++ show err
+  Right val -> "Found value"
 
 main :: IO ()
 main = do
-  args <- getArgs
-  putStrLn ("Hello, " ++ args !! 0)
+  putStrLn (">")
+  expr <- getLine
+  putStrLn (readExpr expr)
